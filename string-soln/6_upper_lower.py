da03a32e17176a7d2327b52d99f97f8cf4c2deab
rohan vetale <vetalerohan@gmail.com>
2024-01-07 15:14:07 +0530

    [Rohan] Add: Program to convert an input string to upper case and lower case

---
+++ string-soln/6_upper_lower.py
@@ -0,0 +1,17 @@
+'''
+
+@Author: Rohan Vetale
+
+@Date: 2024-01-07 09:00:30
+
+@Last Modified by: Rohan Vetale
+
+@Last Modified time: 2024-01-07 18:00:30
+
+@Title : Program to print the upper case and lower case of a string provided by user input
+
+'''
+
+input_str = str(input("Enter any string you want : "))
+print(f"Your output string in lower case is {input_str.lower()}")
+print(f"Your output string in upper case is {input_str.upper()}")
