37059be6f39cc1325eb793b6ef1998722f343d25
rohan vetale <vetalerohan@gmail.com>
2024-01-07 11:15:19 +0530

    [Rohan] Add: Program to print the length of a given string

---
+++ string-soln/1_len.py
@@ -0,0 +1,16 @@
+'''
+
+@Author: Rohan Vetale
+
+@Date: 2024-01-07 09:00:30
+
+@Last Modified by: Rohan Vetale
+
+@Last Modified time: 2024-01-07 18:00:30
+
+@Title : Print the length of a given string
+
+'''
+
+str1 = "Hello World"
+print(len(str1))
