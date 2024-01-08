13d68f6b697bdb37fd362d34fee4f68a43bbea1a
rohan vetale <vetalerohan@gmail.com>
2024-01-08 17:00:12 +0530

    [Rohan] Add: Program to print sum of all the elements in the list

---
+++ list-soln/1_list_sum.py
@@ -0,0 +1,20 @@
+'''
+
+@Author: Rohan Vetale
+
+@Date: 2024-01-08 09:00:30
+
+@Last Modified by: Rohan Vetale
+
+@Last Modified time: 2024-01-08 18:00:30
+
+@Title : Program to print the sum of all elements in the list
+
+'''
+list1 = [33, 14, 512, 55, 99]
+
+sum = 0
+for item in list1:
+    sum += item
+
+print(f"Sum of all the items in the list is {sum}")
