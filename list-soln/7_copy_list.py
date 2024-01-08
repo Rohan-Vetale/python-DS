dad12a9ddab1f45b9c452d88655aa7b2a42015e3
rohan vetale <vetalerohan@gmail.com>
2024-01-08 17:25:04 +0530

    [Rohan] Add: Program to copy a list into another list

---
+++ list-soln/7_copy_list.py
@@ -0,0 +1,16 @@
+'''
+
+@Author: Rohan Vetale
+
+@Date: 2024-01-08 09:00:30
+
+@Last Modified by: Rohan Vetale
+
+@Last Modified time: 2024-01-08 18:00:30
+
+@Title : Program to copy a list into a new list
+
+'''
+new_list =  [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+copied_list = new_list.copy()
+print(copied_list)
