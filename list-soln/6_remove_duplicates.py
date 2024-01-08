b812c019e98fea664af2541907fd9d6b378cadb5
rohan vetale <vetalerohan@gmail.com>
2024-01-08 17:24:17 +0530

    [Rohan] Add: Program to remove duplicates from the given list

---
+++ list-soln/6_remove_duplicates.py
@@ -0,0 +1,17 @@
+'''
+
+@Author: Rohan Vetale
+
+@Date: 2024-01-08 09:00:30
+
+@Last Modified by: Rohan Vetale
+
+@Last Modified time: 2024-01-08 18:00:30
+
+@Title : Program to remove duplicates from a given list
+
+'''
+list6 = [88, 14, 22, 39, 85, 29, 22, 29, 85]
+new_dict = set(list6)
+non_dupli_list = list(new_dict)
+print(non_dupli_list)
