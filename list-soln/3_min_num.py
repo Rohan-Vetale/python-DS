1437241c4c430c9aa1be32a1cea7318b2089b0f3
rohan vetale <vetalerohan@gmail.com>
2024-01-08 17:02:43 +0530

    [Rohan] Add: Program to print the minimum number from the given list

---
+++ list-soln/3_min_num.py
@@ -0,0 +1,15 @@
+'''
+
+@Author: Rohan Vetale
+
+@Date: 2024-01-08 09:00:30
+
+@Last Modified by: Rohan Vetale
+
+@Last Modified time: 2024-01-08 18:00:30
+
+@Title : Program to print minimum number from the list
+
+'''
+list3 = [88, 14, 22, 39, 85, 29]
+print("Smallest number from the list is",sorted(list3)[0])
