90f5fdc64b5edf2fa0e99287bddbca1e5815e4ff
rohan vetale <vetalerohan@gmail.com>
2024-01-09 12:57:27 +0530

    [Rohan] Add: Program to union a set

---
+++ set-soln/7_union_set.py
@@ -0,0 +1,17 @@
+'''
+
+@Author: Rohan Vetale
+
+@Date: 2024-01-09 09:00:30
+
+@Last Modified by: Rohan Vetale
+
+@Last Modified time: 2024-01-09 18:00:30
+
+@Title :  Python program to create a union of sets
+
+'''
+my_set = {'carrot', 'banana', 'apple'}
+your_set = {'carrot', 'banana', 'apple', 'jackfruit', 'pineapple'}
+union_set = my_set.union(your_set)
+print(f"Union of {my_set} and {your_set} is {union_set}")
