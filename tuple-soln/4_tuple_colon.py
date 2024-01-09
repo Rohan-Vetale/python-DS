754484b867e08fdd8b924c2d9684bdc0e1319c81
rohan vetale <vetalerohan@gmail.com>
2024-01-09 11:00:07 +0530

    [Rohan] Add: Program to add tuple in another tuple

---
+++ tuple-soln/4_tuple_colon.py
@@ -0,0 +1,17 @@
+'''
+
+@Author: Rohan Vetale
+
+@Date: 2024-01-09 09:00:30
+
+@Last Modified by: Rohan Vetale
+
+@Last Modified time: 2024-01-09 18:00:30
+
+@Title : Python program to Create the colon of a tuple
+
+'''
+
+original_tuple = (1, 2, 3, 4, 5)
+colon_tuple = original_tuple + (6,)
+print("Colon Tuple:", colon_tuple)
