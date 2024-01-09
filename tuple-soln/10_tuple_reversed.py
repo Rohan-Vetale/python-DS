b4d842c69d6372bce3d6fc92c9f184c2111bb7b4
rohan vetale <vetalerohan@gmail.com>
2024-01-09 11:09:02 +0530

    [Rohan] Add: Program to reverse a given tuple

---
+++ tuple-soln/10_tuple_reversed.py
@@ -0,0 +1,18 @@
+'''
+
+@Author: Rohan Vetale
+
+@Date: 2024-01-09 09:00:30
+
+@Last Modified by: Rohan Vetale
+
+@Last Modified time: 2024-01-09 18:00:30
+
+@Title : Python program to reverse a tuple
+
+'''
+
+
+original_tuple = (1, 2, 3, 4, 5)
+reversed_tuple = tuple(reversed(original_tuple))
+print("Reversed Tuple:", reversed_tuple)
