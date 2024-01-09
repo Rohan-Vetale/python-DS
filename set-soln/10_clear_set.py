a55c14f9bc46d4628f9442adbb44e4c9d8b498af
rohan vetale <vetalerohan@gmail.com>
2024-01-09 12:59:44 +0530

    [Rohan] Add: Program to clear a set completely

---
+++ set-soln/10_clear_set.py
@@ -0,0 +1,17 @@
+'''
+
+@Author: Rohan Vetale
+
+@Date: 2024-01-09 09:00:30
+
+@Last Modified by: Rohan Vetale
+
+@Last Modified time: 2024-01-09 18:00:30
+
+@Title :  Python program to clear a set
+
+''' 
+your_set = {'carrot', 'banana', 'apple', 'jackfruit', 'pineapple'}
+print(f"Original set was {your_set}")
+your_set.clear()
+print(f"Set after clearing is {your_set}")
