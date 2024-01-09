0ee731c875a067cf42ba713448ab2eb985ff9c4c
rohan vetale <vetalerohan@gmail.com>
2024-01-09 13:00:13 +0530

    [Rohan] Add: Program to create a frozenset

---
+++ set-soln/11_create_frozenset.py
@@ -0,0 +1,16 @@
+'''
+
+@Author: Rohan Vetale
+
+@Date: 2024-01-09 09:00:30
+
+@Last Modified by: Rohan Vetale
+
+@Last Modified time: 2024-01-09 18:00:30
+
+@Title :  Python program to use a frozenset
+
+''' 
+animals = frozenset(["cat", "dog", "lion"])
+print("cat" in animals) 
+print("elephant" in animals)
