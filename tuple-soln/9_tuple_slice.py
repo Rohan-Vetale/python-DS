f5ab71b5d8a28e397646ec793dfdf416dd85466a
rohan vetale <vetalerohan@gmail.com>
2024-01-09 11:08:25 +0530

    [Rohan] Add: Program to slice a tuple

---
+++ tuple-soln/9_tuple_slice.py
@@ -0,0 +1,17 @@
+'''
+
+@Author: Rohan Vetale
+
+@Date: 2024-01-09 09:00:30
+
+@Last Modified by: Rohan Vetale
+
+@Last Modified time: 2024-01-09 18:00:30
+
+@Title : Python program to slice a tuple
+
+'''
+
+original_tuple = (1, 2, 3, 4, 5)
+sliced_tuple = original_tuple[1:4]
+print("Sliced Tuple:", sliced_tuple)
