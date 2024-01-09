2402a88b8496dec61f9b4fa5603d64b0d2b05d93
rohan vetale <vetalerohan@gmail.com>
2024-01-09 10:58:23 +0530

    [Rohan] Add: Program to unpack a tuple into several variables

---
+++ tuple-soln/3_tuple_unpack.py
@@ -0,0 +1,16 @@
+'''
+
+@Author: Rohan Vetale
+
+@Date: 2024-01-09 09:00:30
+
+@Last Modified by: Rohan Vetale
+
+@Last Modified time: 2024-01-09 18:00:30
+
+@Title : Python program to unpack a tuple into several variables
+
+'''
+my_tuple = (10, 20, 30)
+var1, var2, var3 = my_tuple
+print("Unpacked variables:", var1, var2, var3)
