15e0ca982585f0713f6a28edd2749b376d861fc0
rohan vetale <vetalerohan@gmail.com>
2024-01-09 10:56:06 +0530

    [Rohan] Add: Python program to create a tuple

---
+++ tuple-soln/1_create_tuple.py
@@ -0,0 +1,17 @@
+'''
+
+@Author: Rohan Vetale
+
+@Date: 2024-01-09 09:00:30
+
+@Last Modified by: Rohan Vetale
+
+@Last Modified time: 2024-01-09 18:00:30
+
+@Title : Python program to create a tuple
+
+'''
+#Python program to create a tuple.
+
+my_tuple = (1, 2, 3, 4, 5)
+print("Tuple:", my_tuple)
