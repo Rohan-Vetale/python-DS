d2fe53f4f478765b5c799cefde30dfdeea51b910
rohan vetale <vetalerohan@gmail.com>
2024-01-09 11:07:09 +0530

    [Rohan] Add: Program to convert a list to tuple

---
+++ tuple-soln/7_list_to_tuple.py
@@ -0,0 +1,17 @@
+'''
+
+@Author: Rohan Vetale
+
+@Date: 2024-01-09 09:00:30
+
+@Last Modified by: Rohan Vetale
+
+@Last Modified time: 2024-01-09 18:00:30
+
+@Title : Python program to Convert a list to a tuple
+
+'''
+
+my_list = [1, 2, 3, 4, 5]
+converted_tuple = tuple(my_list)
+print("Converted Tuple:", converted_tuple)
