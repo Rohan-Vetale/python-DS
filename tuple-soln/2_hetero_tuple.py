e3be4a7620213a1facbf1497331cab760add4666
rohan vetale <vetalerohan@gmail.com>
2024-01-09 10:57:36 +0530

    [Rohan] Add: Program to create a tuple with heterogenous data type

---
+++ tuple-soln/2_hetero_tuple.py
@@ -0,0 +1,15 @@
+'''
+
+@Author: Rohan Vetale
+
+@Date: 2024-01-09 09:00:30
+
+@Last Modified by: Rohan Vetale
+
+@Last Modified time: 2024-01-09 18:00:30
+
+@Title : Python program to create a tuple with different data types
+
+'''
+mixed_tuple = (1, "hello", 3.14, True)
+print("Mixed Tuple:", mixed_tuple)
