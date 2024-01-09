585db87820f34a76c1406dcaac122f5e7d97dd4f
rohan vetale <vetalerohan@gmail.com>
2024-01-09 12:52:06 +0530

    [Rohan] Add: Python program to create a set

---
+++ set-soln/1_create_set.py
@@ -0,0 +1,15 @@
+'''
+
+@Author: Rohan Vetale
+
+@Date: 2024-01-09 09:00:30
+
+@Last Modified by: Rohan Vetale
+
+@Last Modified time: 2024-01-09 18:00:30
+
+@Title : Python program to create a set
+
+'''
+new_set = {1,2,3,4,5,6,7}
+print(new_set)
